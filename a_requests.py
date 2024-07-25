17123997edc89bdff4e4501d33c1e6fa46a93135
aytugan242 <147434124+aytugan242@users.noreply.github.com>
2024-07-25 13:15:23 +0300

    Update a_requests.py

--- a_requests.py
+++ a_requests.py
@@ -11,10 +11,43 @@ async def get_people(person_id, http_session):
 
     response = await http_session.get(f"https://swapi.dev/api/people/{person_id}/")
     json_data = await response.json()
+
+# данные по films
+    films_urls = json_data.get('films', [])
+    tasks_films = []
+    for film_url in films_urls:
+        task = asyncio.create_task(fetch_film_title(film_url, http_session))
+        tasks_films.append(task)
+    film_titles = await asyncio.gather(*tasks_films)
+
+# данные по species
+    species_urls = json_data.get('species', [])
+    tasks_species = []
+    for n_url in species_urls:
+        task = asyncio.create_task(fetch_name(n_url, http_session))
+        tasks_species.append(task)
+    spec_titles = await asyncio.gather(*tasks_species)
+
+# данные по starships
+    starships_urls = json_data.get('starships', [])
+    tasks_starships = []
+    for n_url in starships_urls:
+        task = asyncio.create_task(fetch_name(n_url, http_session))
+        tasks_starships.append(task)
+    starship_titles = await asyncio.gather(*tasks_starships)
+
+# данные по vehicles
+    vehicles_urls = json_data.get('vehicles', [])
+    tasks_vehicles = []
+    for n_url in vehicles_urls:
+        task = asyncio.create_task(fetch_name(n_url, http_session))
+        tasks_vehicles.append(task)
+    vehicl_titles = await asyncio.gather(*tasks_vehicles)
+
     return {
         'birth_year': json_data.get('birth_year', ''),
         'eye_color': json_data.get('eye_color', ''),
-        'films': ', '.join(json_data.get('films', '')),
+        'films': ', '.join(film_titles),
         'gender': json_data.get('gender', ''),
         'hair_color': json_data.get('hair_color', ''),
         'height': json_data.get('height', ''),
@@ -22,10 +55,21 @@ async def get_people(person_id, http_session):
         'mass': json_data.get('mass', ''),
         'name': json_data.get('name', ''),
         'skin_color': json_data.get('skin_color', ''),
-        'species': ', '.join(json_data.get('species', '')),
-        'starships': ', '.join(json_data.get('starships', '')),
-        'vehicles': ', '.join(json_data.get('vehicles', ''))
+        'species': ', '.join(spec_titles),
+        'starships': ', '.join(starship_titles),
+        'vehicles': ', '.join(vehicl_titles)
     }
+
+# вспомогательные функции для обработки данных по URL
+async def fetch_film_title(film_url, http_session):
+    response = await http_session.get(film_url)
+    film_data = await response.json()
+    return film_data['title']
+
+async def fetch_name(n_url, http_session):
+    response = await http_session.get(n_url)
+    film_data = await response.json()
+    return film_data['name']
 
 async def insert(jsons_list):
     async with Session() as db_session:
